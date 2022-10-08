7f8a056a1b4c41fb4a98555451e3f35f47432df6
Defaul33t <carlosgallarde01@gmail.com>
2022-10-08 16:13:29 +0200

    backend

--- frontend/gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: ["gatsby-plugin-postcss"],
-  pathPrefix: "/projects/deliverix/",
-};
--- frontend/src/components/UI/WrapperFood.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WrapperFood = (props) => {
-  return <div className="max-w-6xl mx-auto">{props.children}</div>;
-};
-
-export default WrapperFood;
